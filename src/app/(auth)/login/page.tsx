e302187ef6d08016d1d5c827d08ef87a989509a1
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-06-04 16:03:35 +0700

    Auth front end

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+const LoginPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-full h-full grid lg:grid-cols-2 p-4">
+        <div className="max-w-xs m-auto w-full flex flex-col items-center">
+          <p className="mt-4 text-xl font-bold tracking-tight">
+            My Store Login
+          </p>
+
+          <Separator className="my-7 w-full flex items-center justify-center"/>
+
+          <Form {...form}>
+            <form
+              className="w-full space-y-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mt-4 w-full">
+                Continue
+              </Button>
+            </form>
+          </Form>
+
+          <div className="mt-5 space-y-5">
+            <p className="text-sm text-center">
+              Don&apos;t have an account?
+              <Link href="/signup" className="ml-1 underline text-muted-foreground">
+                Create account
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="bg-muted hidden lg:block rounded-lg" />
+      </div>
+    </div>
+  );
+};
+
+
+export default LoginPage;
